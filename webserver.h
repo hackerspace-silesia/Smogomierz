d8e117eea5790475773b6fd2f914fa9347e4c75f
Blazej Faliszek <blazejfaliszek@gmail.com>
2018-03-09 12:39:09 +0100

    update

--- webserver.h
+++ webserver.h
@@ -1,6 +1,5 @@
-#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include "pms.h" // https://github.com/fu-hsi/PMS
 #include "bme280.h" // https://github.com/zen/BME280_light/blob/master/BME280_t.h
 
-void webserverShowSite(WiFiServer &server, BME280<BME280_C, BME280_ADDRESS> &bme, const PMS::DATA &pms);
-
+//void webserverShowSite(WiFiServer &server, BME280<BME280_C, BME280_ADDRESS> &bme, const PMS::DATA &pms);

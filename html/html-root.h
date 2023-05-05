6dd6d2497a556b2d0bf51553370a142569800cd1
Blazej Faliszek <blazejfaliszek@gmail.com>
2023-05-05 09:39:48 +0200

    update bootstrap

--- html/html-root.h
+++ html/html-root.h
@@ -34,11 +34,10 @@ static const char WEB_ROOT_PAGE_ALL[] PROGMEM = R"rawliteral(<html lang='%{Langu
 										  		document.write(new Date().getFullYear());
 											</script>
 											</center></div></main></form>
-					<script src='https://code.jquery.com/jquery-3.4.1.slim.min.js' integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n' crossorigin='anonymous'></script>
-					<script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin='anonymous'></script>
-					<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossorigin='anonymous'></script>
+					<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+					<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+					<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 					</body></html>)rawliteral";
-
 			
 static const char WEB_ROOT_PAGE_MEASUREMENTS_THP1[] PROGMEM = R"rawliteral(<h2>{TEXT_WEATHER}:</h2>
 					<h3>{TEXT_TEMPERATURE}: {Temperature} °C</h3>
